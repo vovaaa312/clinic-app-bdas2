7e14e48a434907d4b22f3e48ca070ef1487cbe6d
Konstantin Rostovtcev <roska071@gmail.com>
2023-12-16 16:58:41 +0100

    funkce vypocitat_prumerny_vek_pacientu

--- src/main/java/com/clinicappbdas2/service/PacientService.java
+++ src/main/java/com/clinicappbdas2/service/PacientService.java
@@ -4,6 +4,7 @@ import com.clinicappbdas2.model.Pacient;
 import com.clinicappbdas2.service.repository.PacientRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
 public class PacientService {
 
     private final PacientRepository pacientRepository;
+    private final JdbcTemplate jdbcTemplate;
 
     public List<Pacient> getAll() {
         return pacientRepository.getAll();
@@ -43,5 +45,12 @@ public class PacientService {
 
     public List<Pacient> getByCisloTelefonu(Integer cisloTelefonu) {
         return pacientRepository.getByCisloTelefonu(cisloTelefonu);
+    }
+
+    public double calculateAverageAge() {
+        return jdbcTemplate.queryForObject(
+                "SELECT vypocitat_prumerny_vek_pacientu() FROM dual",
+                Double.class
+        );
     }
 }
